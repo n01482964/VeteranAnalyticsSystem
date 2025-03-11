7b264e14eb37fac653ad48f741768d9beb26d164
John <n01482964@unf.edu>
2025-03-11 10:17:17 -0400

    Updated database to local database

---
+++ Controllers/DataImportController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using VeteranAnalyticsSystem.Data;
+using VeteranAnalyticsSystem.Services;
+
+namespace VeteranAnalyticsSystem.Controllers
+{
+    public class DataImportController : Controller
+    {
+        private readonly GratitudeAmericaDbContext _context;
+        private readonly CsvImporter _csvImporter;
+
+        public DataImportController(GratitudeAmericaDbContext context)
+        {
+            _context = context;
+            _csvImporter = new CsvImporter();
+        }
+
+        // This action triggers the import from the CSV file.
+        // In production, you might secure this endpoint with authorization.
+        [HttpPost]
+        public IActionResult ImportCsv()
+        {
+            // Construct the file path for the CSV file stored in the DataFiles folder.
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "DataFiles", "Master Applicant List.csv");
+
+            // Import the records from CSV
+            var veterans = _csvImporter.ImportVeterans(filePath);
+
+            // Save the records to the database
+            _context.Veterans.AddRange(veterans);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Veterans");
+        }
+    }
+}
--- Migrations/20250311045112_InitialCreate.Designer.cs
+++ Migrations/20250311045112_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VeteranAnalyticsSystem.Data;
 
@@ -11,9 +12,11 @@ using VeteranAnalyticsSystem.Data;
 namespace VeteranAnalyticsSystem.Migrations
 {
     [DbContext(typeof(GratitudeAmericaDbContext))]
-    partial class GratitudeAmericaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250311045112_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
